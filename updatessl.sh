ae9b340bfe82b902da664129e3fda3c82a509e86
Guillaume Augais <guillaume@augais.fr>
2021-01-06 15:06:54 +0100

    feat: updatessl handles other .conf file in /etc/nginx/conf.d

--- updatessl.sh
+++ updatessl.sh
@@ -5,13 +5,16 @@ _SCRIPT_="$0"
 ACME_BIN="/acme.sh/acme.sh --home /acme.sh --config-home /acmecerts"
 
 DEFAULT_CONF="/etc/nginx/conf.d/default.conf"
-
+CONF_DIR="/etc/nginx/conf.d"
 
 CERTS="/etc/nginx/certs"
 
 
 updatessl() {
   nginx -t && nginx -s reload
+
+  echo "Handling default conf file : $DEFAULT_CONF"
+
   if grep ACME_DOMAINS $DEFAULT_CONF ; then
     for d_list in $(grep ACME_DOMAINS $DEFAULT_CONF | cut -d ' ' -f 2);
     do
@@ -29,6 +32,40 @@ updatessl() {
   else
     echo "skip updatessl"
   fi
+
+  #
+	# OTHER CONF FILES
+	#
+	for filename in $CONF_DIR/*.conf; do
+		echo "Handling other conf file : $filename"
+
+		if [ "$filename" = "$DEFAULT_CONF" ] ; then
+			# do nothing : skip default conf (already updated)
+			echo "Skipping default conf (already updated)"
+		else
+			if grep ACME_DOMAINS $filename ; then
+				for d_list in $(grep ACME_DOMAINS $filename | cut -d ' ' -f 2);
+				do
+					d=$(echo "$d_list" | cut -d , -f 1)
+					$ACME_BIN --issue \
+					-d $d_list \
+					--nginx \
+					--fullchain-file "$CERTS/$d.crt" \
+					--key-file "$CERTS/$d.key" \
+					--reloadcmd "nginx -t && nginx -s reload"
+
+					echo "updatessl done for domain $d_list"
+				done
+
+				echo "updatessl done for $filename"
+			else
+				echo "skip updatessl for $filename"
+			fi
+			nginx -t && nginx -s reload
+
+		fi
+	done
+
   nginx -t && nginx -s reload
 }
 
